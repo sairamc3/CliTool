a3c2e7e02633ba17591e9226c82581704d0e9e9b
Sairam Chekuri <sairamc3@gmail.com>
2023-12-23 17:03:44 +0530

    Initial Commit

---
+++ CliTool/src/main/java/com/chekuri/CliTool/CliToolApplication.java
@@ -0,0 +1,13 @@
+package com.chekuri.CliTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliToolApplication.class, args);
+	}
+
+}
---
+++ CliTool/src/test/java/com/chekuri/CliTool/CliToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chekuri.CliTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
